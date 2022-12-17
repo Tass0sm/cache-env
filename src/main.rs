f0fa9cbafbe000c0c0822dec8cd51227a60d72ba
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-12-17 02:04:31 -0500

    Added dependencies and outline for main commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs::File;
+use std::env;
+use std::collections::HashMap;
+use serde_json;
+use serde::{Serialize, Deserialize};
 use clap::{arg, Command};
 
 fn cli() -> Command {
@@ -6,6 +13,11 @@ fn cli() -> Command {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .allow_external_subcommands(true)
+        .arg(arg!(-f <ENV_FILE> "file for reading and writing layers"))
+        .subcommand(
+            Command::new("init")
+                .about("Saves the current environment as the onion env base.")
+        )
         .subcommand(
             Command::new("save")
                 .about("Saves the current environment in a new layer.")
@@ -13,39 +25,153 @@ fn cli() -> Command {
                 .arg_required_else_help(true),
         )
         .subcommand(
-            Command::new("unload")
-                .about("Removes the named layer.")
+            Command::new("deactivate")
+                .about("Deactivates the named layer.")
                 .arg(arg!(<NAME> "the name for the environment layer to unload")),
         )
         .subcommand(
-            Command::new("reload")
+            Command::new("reactivate")
                 .about("Reapplies the named layer.")
                 .arg(arg!(<NAME> "the name for the environment layer to reload")),
         )
+        .subcommand(
+            Command::new("export")
+                .about("Prints commands for setting the environment.")
+
+        )
+        .subcommand(
+            Command::new("env")
+                .about("Prints the environment colored according to the layers.")
+        )
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                                definitions                                //
+///////////////////////////////////////////////////////////////////////////////
+
+type Env = HashMap<String, String>;
+// type EnvVarDiff = TextDiff<str>;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct EnvLayer {
+    name: String,
+    is_active: bool,
+    // diff: HashMap<String, EnvVarDiff>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct EnvOnion {
+    base: Env,
+    layers: Vec<EnvLayer>
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                                   utils                                   //
+///////////////////////////////////////////////////////////////////////////////
+
+fn get_current_env() -> Env {
+    let mut env = HashMap::new();
+    for (key, value) in env::vars() {
+        env.insert(key, value);
+    }
+
+    return env
+}
+
+fn read_onion(env_file_path: &Path) -> EnvOnion {
+    let mut file = File::open(env_file_path).unwrap();
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+    return serde_json::from_str(&s).unwrap();
+}
+
+fn write_onion(onion: EnvOnion, env_file_path: &Path) {
+    let mut file = File::create(env_file_path).unwrap();
+    let serialized = serde_json::to_string(&onion).unwrap();
+    writeln!(&mut file, "{}", serialized).unwrap();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                                  commands                                 //
+///////////////////////////////////////////////////////////////////////////////
+
+fn init(env_file_path: &Path) {
+    let onion = EnvOnion {
+        base: get_current_env(),
+        layers: vec![]
+    };
+
+    write_onion(onion, env_file_path);
+}
+
+fn save(env_file_path: &Path, name: &str) {
+    let mut onion = read_onion(env_file_path);
+    let new_layer = EnvLayer {
+        name: name.to_string(),
+        is_active: true,
+    };
+
+    onion.layers.push(new_layer);
+    write_onion(onion, env_file_path)
+}
+
+fn deactivate(env_file_path: &Path, name: &str) {
+    let mut onion = read_onion(env_file_path);
+    let index = onion.layers.iter().position(|l| l.name == name).unwrap();
+
+    onion.layers[index].is_active = false;
+    write_onion(onion, env_file_path)
+}
+
+fn reactivate(env_file_path: &Path, name: &str) {
+    let mut onion = read_onion(env_file_path);
+    let index = onion.layers.iter().position(|l| l.name == name).unwrap();
+
+    onion.layers[index].is_active = true;
+    write_onion(onion, env_file_path)
+}
+
+fn export(env_file_path: &Path) {
+    let onion = read_onion(env_file_path);
+
+    for (key, value) in onion.base {
+        println!("{key}={value}");
+    }
+}
+
+fn env(env_file_path: &Path) {
+    let onion = read_onion(env_file_path);
+
+    for (key, value) in onion.base {
+        println!("{key}={value}");
+    }
 }
 
 fn main() {
-   let matches = cli().get_matches();
+    let matches = cli().get_matches();
 
-   match matches.subcommand() {
-       Some(("save", sub_matches)) => {
-           println!(
-               "Save {}",
-               sub_matches.get_one::<String>("NAME").expect("required")
-           );
-       }
-       Some(("unload", sub_matches)) => {
-           println!(
-               "Unload {}",
-               sub_matches.get_one::<String>("NAME").expect("required")
-           );
-       }
-       Some(("reload", sub_matches)) => {
-           println!(
-               "Reload {}",
-               sub_matches.get_one::<String>("NAME").expect("required")
-           );
-       }
-       _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
-   }
+    let env_file_arg = matches.get_one::<String>("ENV_FILE").expect("required");
+    let env_file_path = Path::new(env_file_arg);
+
+    match matches.subcommand() {
+        Some(("init", _sub_matches)) => {
+            init(env_file_path);
+        }
+        Some(("save", sub_matches)) => {
+            let name = sub_matches.get_one::<String>("NAME").expect("required");
+            save(env_file_path, name);
+        }
+        Some(("deactivate", sub_matches)) => {
+            let name = sub_matches.get_one::<String>("NAME").expect("required");
+            deactivate(env_file_path, name);
+        }
+        Some(("reactivate", sub_matches)) => {
+            let name = sub_matches.get_one::<String>("NAME").expect("required");
+            reactivate(env_file_path, name);
+        }
+        Some(("env", _sub_matches)) => {
+            env(env_file_path);
+        }
+        _ => unreachable!(),
+    }
 }
